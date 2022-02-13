5e89018191e712ce3a6f5499c6488d0a39d5cb24
Nisa Yıldırım <nisayildrm96@gmail.com>
2022-02-13 10:59:44 +0300

    first commit

---
+++ DiagonalDifference.js
@@ -0,0 +1,16 @@
+
+//Diagonal Difference Solution 
+
+
+function diagonalDifference(arr) {
+
+    const length = arr.length;
+    let leftDiagonal=0;
+    let rightDiagonal=0;
+
+    for (let i = 0; i < arr.length ; i++) {
+        leftDiagonal += arr[i][i];
+        rightDiagonal += arr[i][length-1-i];
+    }
+    return Math.abs(leftDiagonal - rightDiagonal);
+}
