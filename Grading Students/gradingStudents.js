d5d98ecba1c4aded39f4dc88d512db8310d8ad33
Nisa Yıldırım <nisayildrm96@gmail.com>
2022-02-19 15:11:04 +0300

    main

---
+++ Grading Students/gradingStudents.js
@@ -0,0 +1,9 @@
+function gradingStudents(grades) {
+    return grades.map((n) => {
+        let diff = 5 - (n % 5);
+        if(diff < 3 && n > 37) {
+            n += diff;
+        }
+        return n;
+    })
+}
