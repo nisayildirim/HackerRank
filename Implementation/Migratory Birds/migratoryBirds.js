196cc1dc91f5b0ad9ee6befb41dd0bf66c0ccf8b
Nisa Yıldırım <nisayildrm96@gmail.com>
2022-03-09 21:25:56 +0300

    main

---
+++ Implementation/Migratory Birds/migratoryBirds.js
@@ -0,0 +1,16 @@
+// migratory Birds Solution :)
+
+function migratoryBirds(arr) {
+    let largest = 1,
+        counter = largest,
+        birdType = 0;
+    arr.sort();
+    for (let i=0; i<arr.length ; i++) {
+        largest = (arr.lastIndexOf(arr[i])-arr.indexOf(arr[i]))+1; //lastIndexOf() returns the index (position) specified value in a string.
+        if (largest>counter) {                                     //indexOf() method returns the position in a string.
+            counter=largest ;
+            birdType = arr[i];
+       }
+    }
+return birdType;
+}
