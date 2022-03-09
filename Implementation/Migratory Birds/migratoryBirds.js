856887c44067190bfda63c44d0bb52c226e71d0f
Nisa Yıldırım <nisayildrm96@gmail.com>
2022-03-09 21:32:59 +0300

    main

--- Implementation/Migratory Birds/migratoryBirds.js
+++ Implementation/Migratory Birds/migratoryBirds.js
@@ -6,8 +6,8 @@ function migratoryBirds(arr) {
         birdType = 0;
     arr.sort();
     for (let i=0; i<arr.length ; i++) {
-        largest = (arr.lastIndexOf(arr[i])-arr.indexOf(arr[i]))+1; //lastIndexOf() returns the index (position) specified value in a string.
-        if (largest>counter) {                                     //indexOf() method returns the position in a string.
+        largest = (arr.lastIndexOf(arr[i])-arr.indexOf(arr[i]))+1; //lastIndexOf() returns the index (position) specified value in a string. last
+        if (largest>counter) {                                     //indexOf() method returns the position in a string. first
             counter=largest ;
             birdType = arr[i];
        }
