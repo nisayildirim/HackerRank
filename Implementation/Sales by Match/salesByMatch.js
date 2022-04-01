66b5cb0ecef49dc243a1a95df6cdaff90cbc3e84
Nisa Yıldırım <nisayildrm96@gmail.com>
2022-04-01 09:19:01 +0300

    main

---
+++ Implementation/Sales by Match/salesByMatch.js
@@ -0,0 +1,15 @@
+//Sales by Match Solution :)
+
+function sockMerchant(n,ar) {
+    let pairCounter = 0;
+    let search = new Set();
+    for(const sock of ar) {
+        if (search.has(sock)){
+            pairCounter++;
+            search.delete(sock);
+        }else{
+            search.add(sock);
+        }
+    }
+    return pairCounter;
+}
