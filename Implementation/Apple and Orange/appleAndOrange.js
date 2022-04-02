9d5da255cfa82cd6ab01ce60d3e240a6cc9e46c5
Nisa Yıldırım <nisayildrm96@gmail.com>
2022-04-02 22:34:27 +0300

    main

--- Implementation/Apple and Orange/appleAndOrange.js
+++ Implementation/Apple and Orange/appleAndOrange.js
@@ -1,19 +1,18 @@
 // Apple and Orange Solution :)
 
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-    let appleCounter = 0;
-    let orangeCounter = 0;
-    for (let i = 0; i < apples.length; i++) {
-        if (a + apples[i]>= s && a + apples[i]<= t) {
-            appleCounter++;
-        }
+  let appleCounter = 0;
+  let orangeCounter = 0;
+  for (let i = 0; i < apples.length; i++) {
+    if (a + apples[i] >= s && a + apples[i] <= t) {
+      appleCounter++;
     }
-    for (let j = 0; j < oranges.length; j++) {
-        if (b + oranges[j]>= s && b + oranges[j]<= t ) {
-            orangeCounter++;
-        }
+  }
+  for (let j = 0; j < oranges.length; j++) {
+    if (b + oranges[j] >= s && b + oranges[j] <= t) {
+      orangeCounter++;
     }
-    console.log(appleCounter);
-    console.log(orangeCounter);
+  }
+  console.log(appleCounter);
+  console.log(orangeCounter);
 }
-
--- Implementation/Migratory Birds/migratoryBirds.js
+++ Implementation/Migratory Birds/migratoryBirds.js
@@ -1,16 +1,17 @@
 // migratory Birds Solution :)
 
 function migratoryBirds(arr) {
-    let largest = 1,
-        counter = largest,
-        birdType = 0;
-    arr.sort();
-    for (let i=0; i<arr.length ; i++) {
-        largest = (arr.lastIndexOf(arr[i])-arr.indexOf(arr[i]))+1; //lastIndexOf() returns the index (position) specified value in a string. last
-        if (largest>counter) {                                     //indexOf() method returns the position in a string. first
-            counter=largest ;
-            birdType = arr[i];
-       }
+  let largest = 1,
+    counter = largest,
+    birdType = 0;
+  arr.sort();
+  for (let i = 0; i < arr.length; i++) {
+    largest = arr.lastIndexOf(arr[i]) - arr.indexOf(arr[i]) + 1; //lastIndexOf() returns the index (position) specified value in a string. last
+    if (largest > counter) {
+      //indexOf() method returns the position in a string. first
+      counter = largest;
+      birdType = arr[i];
     }
-return birdType;
+  }
+  return birdType;
 }
